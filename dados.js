f26e28da18cd9bf1083de45895afc4669fc444ae
Lyly⁶³ ⚢ <160618680+Lyyyat@users.noreply.github.com>
2025-06-02 23:33:02 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,85 +1,85 @@
 let dados = [
-    {   nome : "Face (Jimin)",
-        descricao : "Face é o álbum de estreia do cantor sul-coreano Jimin, do BTS. Lançado em 24 de março de 2023, pela Big Hit Music, é principalmente um disco de pop, hip hop e R&B, com Pdogg, Ghstloop e Evan responsáveis pela produção.",
-        link : "https://ibighit.com/bts/eng/discography/jimin/detail/face/",
-        img: "img/face.jpg" 
+    {   nome : "HIRONO (ECHO)",
+        descricao : "Hirono Pop Mart Blind Box Echo Series.",
+        img: "img/hironoecho.jpg",
+        categoria: "Hirono"
     },
     {
-        nome : "Muse (Jimin)",
-        descricao : "Muse é o segundo álbum de estúdio do cantor sul-coreano Jimin do BTS. Lançado em 19 de julho de 2024 pela Big Hit Music, é composto por sete músicas, incluindo 'Closer Than This', o single de pré-lançamento 'Smeraldo Garden Marching Band' com Loco, e o single principal 'Who'.",
-        link : "https://ibighit.com/bts/eng/discography/jimin/detail/muse/",
-        img: "img/muse.jpg"
+        nome : "HIRONO (MIME)",
+        descricao : "Hirono Pop Mart Blind Box Mime Series.",
+        img: "img/hironomime.jpg",
+        categoria: "Hirono"
     },
     {
-        nome : "Golden (Jungkook)",
-        descricao : "Golden é o álbum de estreia do cantor sul-coreano Jungkook, do BTS, lançado em 3 de novembro de 2023, através da Big Hit Music. Primariamente um disco pop, foi recebido com avaliações favoráveis dos críticos musicais.",
-        link : "https://ibighit.com/bts/eng/discography/jung-kook/detail/golden/",
-        img: "img/golden.jpg"
+        nome : "HIRONO (MISCHIEF)",
+        descricao : "Hirono Pop Mart Blind Box Mischief Series.",
+        img: "img/hironomis.jpg",
+        categoria: "Hirono"
     },
     {
-        nome : "Happy (Jin)",
-        descricao : "Happy é o mini-álbum de estreia do cantor sul-coreano Jin do grupo BTS, lançado em 15 de novembro de 2024 pela gravadora Big Hit Music.",
-        link : "https://ibighit.com/bts/eng/discography/jin/detail/happy/",
-        img: "img/happy.jpg"
+        nome : "HIRONO (RESHAPE)",
+        descricao : "Hirono Pop Mart Blind Box Reshape Series.",
+        img: "img/hironoresh.jpg",
+        categoria: "Hirono"
     },
     {
-        nome : "Hope World (J-hope)",
-        descricao : "Hope World é a mixtape de estreia do rapper sul-coreano J-Hope. Foi lançado em 2 de março de 2018 pela gravadora Big Hit Entertainment, junto com o single 'Daydream'.",
-        link : "https://ibighit.com/bts/eng/discography/j-hope/detail/hope-world/",
-        img: "img/hope world.jpg"
+        nome : "HIRONO (SHELTER)",
+        descricao : "Hirono Pop Mart Blind Box Shelter Series.",
+        img: "img/hironoshelter.jpg",
+        categoria: "Hirono"
     },
     {
-        nome : "Jack in the Box (J-hope)",
-        descricao : "Jack in the Box é o álbum de estreia do rapper sul-coreano J-Hope, lançado em 15 de julho de 2022, pela Big Hit Music. Ele contém 10 faixas, incluindo o single principal, 'More'.",
-        link : "https://ibighit.com/bts/eng/discography/j-hope/detail/jack-in-the-box/index.html",
-        img: "img/jitb.jpg"
+        nome : "HIRONO (THE OTHER ONE)",
+        descricao : "Hirono Pop Mart Blind Box The Other One Series.",
+        img: "img/hironotoo.jpg",
+        categoria: "Hirono"
     },
     {
-        nome : "Hope on the Street Vol. 1 (J-hope)",
-        descricao : "Hope on the Street Vol. 1 é a primeira trilha sonora e EP do rapper e cantor e compositor sul-coreano J-Hope do BTS, lançada em 29 de março de 2024 pela Big Hit Music.",
-        link : "https://ibighit.com/bts/eng/discography/j-hope/detail/hope-on-the-street/index.html",
-        img: "img/hots.jpg"
+        nome : "DIMOO (ANIMAL KINGDOM)",
+        descricao : "Dimoo Pop Mart Blind Box Animal Kingdom Series.",
+        img: "img/dimooak.jpg",
+        categoria: "Dimoo"
     },
     {
-        nome : "Agust D (Agust D)",
-        descricao : "Agust D é a mixtape de estreia do rapper sul coreano de mesmo nome, mais conhecido como Suga do grupo BTS. Foi lançado em 15 de agosto de 2016 pela Big Hit Entertainment através da plataforma online de publicação de músicas SoundCloud.",
-        link : "https://ibighit.com/bts/eng/discography/suga/detail/agust-d/",
-        img: "img/agustd.jpg"
+        nome : "DIMOO (BY YOUR SIDE)",
+        descricao : "Dimoo Pop Mart Blind Box By Your Side Series.",
+        img: "img/dimoobys.jpg",
+        categoria: "Dimoo"
     },
     {
-        nome : "D-2 (Agust D)",
-        descricao : "D-2 é a segunda mixtape do rapper sul coreano Agust D, mais conhecido como Suga do grupo BTS. Foi lançada em 22 de maio de 2020 através da Big Hit Entertainment, em conjunto com o single 'Daechwita'.",
-        link : "https://ibighit.com/bts/eng/discography/suga/detail/d-2/",
-        img: "img/d2.jpg"
+        nome : "DIMOO (NOGST)",
+        descricao : "Dimoo Pop Mart Blind Box No One's Gonna Sleep Tonight Series..",
+        img: "img/dimoonogst.jpg",
+        categoria: "Dimoo"
     },
     {
-        nome : "D-Day (Agust D)",
-        descricao : "D-Day é o álbum de estreia do rapper sul-coreano Agust D, mais conhecido como Suga do grupo BTS. Foi lançado em 21 de abril de 2023, pela Big Hit Music, configurando-se como a terceira parcela de uma trilogia de obras que inclui as mixtapes Agust D e D-2.",
-        link : "https://ibighit.com/bts/eng/discography/suga/detail/d-day/",
-        img: "img/dday.jpg"
+        nome : "DIMOO (RETRO)",
+        descricao : "Dimoo Pop Mart Blind Box Retro Series.",
+        img: "img/dimoors.jpg",
+        categoria: "Dimoo"
     },
     {
-        nome : "Layover (V)",
-        descricao : "Layover é o álbum de estreia do cantor sul-coreano V, do BTS. Foi lançado em 8 de setembro de 2023, através da Big Hit Music. O disco foi descrito como um álbum de pop, R&B e jazz 'liderado pelos tons emocionantes da voz de V'.",
-        link : "https://ibighit.com/bts/eng/discography/v/detail/layover/",
-        img: "img/layover.jpg"    
+        nome : "DIMOO (DATING)",
+        descricao : "Dimoo Pop Mart Blind Box Dating Series.",
+        img: "img/dimoodat.jpg",
+        categoria: "Dimoo"    
     },
     {
-        nome : "Mono. (RM)",
-        descricao : "Mono é a segunda mixtape do rapper sul-coreano RM. Foi lançada em 23 de outubro de 2018, pela Big Hit Entertainment. Possui sete músicas e foi acompanhada por um videoclipe animado para a faixa final, 'Forever Rain'.",
-        link : "https://ibighit.com/bts/eng/discography/rm/detail/mono/",
-        img: "img/mono.jpg"
+        nome : "PUCKY (ROTY POTY)",
+        descricao : "Pucky Pop Mart Blind Box Roty Poty Kitty Series.",
+        img: "img/puckynt.jpg",
+        categoria: "Pucky"
     },
     {
-        nome : "Indigo (RM)",
-        descricao : "Indigo é o álbum de estreia do rapper sul-coreano RM, lançado em 2 de dezembro de 2022, pela Big Hit Music. O álbum marca o primeiro trabalho completo do rapper desde Mono.",
-        link : "https://ibighit.com/bts/eng/discography/rm/detail/indigo/",
-        img: "img/indigo.jpg"
+        nome : "PUCKY (HOME TIME)",
+        descricao : "Pucky Pop Mart Blind Box Home Time Series.",
+        img: "img/puckypa.jpg",
+        categoria: "Pucky"
     },
     {
-        nome : "Right Place, Wrong Person (RM)",
-        descricao : "Right Place, Wrong Person é o segundo álbum de estúdio do rapper sul-coreano RM, lançado em 24 de maio de 2024, através da Big Hit Music.",
-        link : "hhttps://ibighit.com/bts/eng/discography/rm/detail/rpwp/",
-        img: "img/rpwp.jpg"
+        nome : "PUCKY (POKO'S ADV)",
+        descricao : "Pucky Pop Mart Blind Box Poko's Adventure Series.",
+        img: "img/puckyrpk.jpg",
+        categoria: "Pucky"
     }
 ];
